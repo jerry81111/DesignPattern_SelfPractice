037ff6097645ae711f088e22147c9601dd3fa72b
Jerry <jerry81111@yahoo.com.tw>
2019-01-07 16:12:22 +0800

    first

---
+++ src/main/java/tw/jerry/designpattern/DesignPatternHeadfirstApplication.java
@@ -0,0 +1,14 @@
+package tw.jerry.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternHeadfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpatternHeadfirstApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/tw/jerry/designpattern/ServletInitializer.java
@@ -0,0 +1,14 @@
+package tw.jerry.designpattern;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DesignpatternHeadfirstApplication.class);
+	}
+
+}
+
